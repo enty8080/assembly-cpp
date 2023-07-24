16ed611c4428697c81f4f545af9237e8858c26c6
Ivan Nikolsky <enty8080@gmail.com>
2023-07-24 22:39:09 +0200

    Update main.cc

--- add/main.cc
+++ add/main.cc
@@ -1,9 +1,9 @@
 #include <iostream>
 
-extern "C" int _add(int, int);
+extern "C" int add(int, int);
 
 int main(void)
 {
-    std::cout << _add(7, 3) << std::endl;
+    std::cout << add(7, 3) << std::endl;
     return 0;
 }
